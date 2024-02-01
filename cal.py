b8ed9eec7f25d5838a3667a440c59df703e62d24
Saravana00007 <141192819+Saravana00007@users.noreply.github.com>
2024-02-01 07:51:10 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,19 @@
+class Cal:
+    a=int(input("Enter no a:"))
+    b=int(input("Enter no b:"))
+    def add(a,b):
+        return a+b
+    def sub(a,b):
+        return a-b
+    def mul(a,b):
+        return a*b
+    def div(a,b):
+        return a/b
+    def rem(a,b):
+        return a//b
+    
+    print(add(a,b))
+    print(sub(a,b))
+    print(mul(a,b))
+    print(div(a,b))
+    print(rem(a,b))
