1f4e8eb8d7085869f40aa3bf9acda93ccd1210ad
Saravana00007 <saravanars43@gmail.com>
2024-02-02 19:59:59 +0530

    Fix:Written some code in inside

---
+++ PD.py
@@ -0,0 +1,6 @@
+#PD is a Pertol and Diesel
+#It is small think or idea
+# In a i taken pertol litr is 114.59 rs
+# single or one line program or code
+
+print(round(1/(114.59/int(input("Enter a amount:"))),2))
